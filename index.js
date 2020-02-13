cbabfb87b522abd23782c0c711e3de40bbcbea31
Florian Wolff <florian.wolff@pinuts.de>
2020-02-13 11:49:01 +0100

    Ohne prettier

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 module.exports = {
     extends: [
         '@pinuts/eslint-config-pinuts-base',
-        'plugin:prettier/recommended',
         'plugin:react/recommended',
         'plugin:jsx-a11y/recommended'
     ],
